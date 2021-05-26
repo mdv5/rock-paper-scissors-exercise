0f39731e03916d51cb781959ab3697fca6860e65
mdv5 <78748987+mdv5@users.noreply.github.com>
2021-05-25 20:53:15 -0400

    Get a random value for computer choice

--- game.py
+++ game.py
@@ -1,16 +1,27 @@
 # game.py
 
+import random # loads the random module
+
+
 print("Rock, Paper, Scissors, Shoot!")
 
+#prompt user to enter a choice and capture their input
 player_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
 print("Player chose: ", player_choice)
 
-# Validate user input
+# validate user input
 if (player_choice == "rock") or (player_choice == "paper") or (player_choice == "scissors"):
     print("VALID. KEEP GOING")
 else:
     print("OOPS, invalid input. Please try again.")
     exit()
+
+# list of valid choices
+valid_choices = ["rock","paper","scissors"]
+
+computer_choice = random.choice(valid_choices)
+
+print("Computer chose: ", computer_choice)
 
 print("THIS IS THE END OF THE GAME. PLEASE PLAY AGAIN.")
