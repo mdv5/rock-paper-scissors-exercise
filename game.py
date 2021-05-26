15e943e5d8d45f132267e9ed36c7ed56d0f868ac
mdv5 <78748987+mdv5@users.noreply.github.com>
2021-05-25 22:34:36 -0400

    Added name with environment variable

--- game.py
+++ game.py
@@ -1,15 +1,22 @@
 # game.py
 
 import random # loads the random module
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
 print("Rock, Paper, Scissors, Shoot!")
+print("----------------------")
+#capture player name
+player_name = os.getenv('PLAYER_NAME')
+print ("Hi ",player_name,". Welcome to my game.")
+print("----------------------")
 
 #prompt user to enter a choice and capture their input
 player_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
-
+print("----------------------")
 print("Player chose: ", player_choice)
-
 # validate user input
 if (player_choice == "rock") or (player_choice == "paper") or (player_choice == "scissors"):
     print("Choice received successfully.")
@@ -21,7 +28,7 @@ else:
 valid_choices = ["rock","paper","scissors"]
 
 computer_choice = random.choice(valid_choices)
-
+print("----------------------")
 print("Computer chose: ", computer_choice)
 
 winner = "" #set the winner to empty before evaluating the winner
@@ -29,20 +36,21 @@ winner = "" #set the winner to empty before evaluating the winner
 #evaluate the winner
 if (player_choice == "rock"):
     if (computer_choice == "scissors"):
-        winner = 'the player'
+        winner = player_name
     elif (computer_choice == "paper"):
         winner = 'the computer'
 elif (player_choice == "scissors"):
     if (computer_choice == "paper"):
-        winner = 'the player'
+        winner = player_name
     elif (computer_choice == "rock"):
         winner = 'the computer'
 elif (player_choice == "paper"):
     if (computer_choice == "scissors"):
         winner = 'the computer'
     elif (computer_choice == "rock"):
-        winner = 'the player'
+        winner = player_name
 
+print("----------------------")
 # if there is a winner say who won, otherwise declare a tie
 if winner:
     print('The winner is', winner)
