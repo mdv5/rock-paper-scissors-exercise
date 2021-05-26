f51560e4f3ad1b5e35c2ebfd4a680026fa9c4906
mdv5 <78748987+mdv5@users.noreply.github.com>
2021-05-25 20:43:03 -0400

    Validated user inputs

--- game.py
+++ game.py
@@ -5,3 +5,12 @@ print("Rock, Paper, Scissors, Shoot!")
 player_choice = input("Please choose one of 'rock', 'paper', 'scissors': ")
 
 print("Player chose: ", player_choice)
+
+# Validate user input
+if (player_choice == "rock") or (player_choice == "paper") or (player_choice == "scissors"):
+    print("VALID. KEEP GOING")
+else:
+    print("OOPS, invalid input. Please try again.")
+    exit()
+
+print("THIS IS THE END OF THE GAME. PLEASE PLAY AGAIN.")
